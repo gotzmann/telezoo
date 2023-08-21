53c9f6e66a7f48d0e3d3f78a2cb5831e88c17b77
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-08-21 23:43:50 +0300

    ..

--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module telezoo
 go 1.20
 
 require (
-	github.com/go-resty/resty/v2 v2.7.0
+	github.com/google/uuid v1.1.2
 	github.com/joho/godotenv v1.5.1
 	gopkg.in/telebot.v3 v3.1.3
 )
-
-require golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
--- main.go
+++ main.go
@@ -1,103 +1,157 @@
 package main
 
 import (
-    "log"
-    "os"
-    "time"
-    "fmt"
-    "net/http"
-    "bytes"
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-    tele "gopkg.in/telebot.v3"
-    "github.com/joho/godotenv"
-    resty "github.com/go-resty/resty/v2"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
 )
 
-func main () {
+type User struct {
+	ID        string // User ID within external system
+	TGID      int64  // User ID within Telegram
+	Mode      string // pro / chat
+	SessionID string // current session
+	Status    string
+}
 
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+type Session struct {
+	UserID    string // User ID within external system
+	TGID      string // User ID within Telegram
+	SessionID string // Unique UUID v4
+	Prompts   []string
+	Outputs   []string
+}
 
-    pref := tele.Settings{
-        Token:  os.Getenv("TELEGRAM_TOKEN"),
-        Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-    }
+var (
+	users    map[int64]User
+	sessions map[string]string
+)
 
-    b, err := tele.NewBot(pref)
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
+func init() {
+	users = make(map[int64]User)
+	sessions = make(map[string]string)
+}
 
-    b.Handle("/hello", func(c tele.Context) error {
-        return c.Send("Привет!")
-    })
+func main() {
 
-    b.Handle(tele.OnText, func(c tele.Context) error {
-        // All the text messages that weren't
-        // captured by existing handlers.
+	fmt.Printf("\nTeleZoo v 0.0 is starting...")
 
-        var (
-            user = c.Sender()
-            text = c.Text()
-        )
-        // Use full-fledged bot's functions
-        // only if you need a result:
-        _, err := b.Send(user, text)
-        if err != nil {
-            return err
-        }
-        //msg = nil
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-        //res, err := http.Get(requestURL)
-	      //if err != nil {
-		    //    fmt.Printf("error making http request: %s\n", err)
-		    //    os.Exit(1)
-	      //}
+	pref := tele.Settings{
+		Token:  os.Getenv("TELEGRAM_TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
 
-        jsonBody := []byte(`{"id": "655ba2e7-ec5f-4a79-b131-f5e855463c88", "prompt": "Hello, Mira"}`)
-        bodyReader := bytes.NewReader(jsonBody)
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-        requestURL := os.Getenv("FAST") + "/jobs"
-        req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
-        if err != nil {
-		        fmt.Printf("client: could not create request: %s\n", err)
-		        os.Exit(1)
-        }
-        req.Header.Set("Content-Type", "application/json")
+	b.Handle("/pro", func(c tele.Context) error {
+		return c.Send("Switched to PRO mode...")
+	})
 
-        client := http.Client{
-            Timeout: 30 * time.Second,
-        }
+	b.Handle("/chat", func(c tele.Context) error {
+		return c.Send("Switched to CHAT mode...")
+	})
 
-        res, err := client.Do(req)
-        if err != nil {
-	          fmt.Printf("client: error making http request: %s\n", err)
-	          os.Exit(1)
-        }
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		// All the text messages that weren't
+		// captured by existing handlers.
 
-        fmt.Printf("%+v", res)
+		var (
+			tgUser = c.Sender()
+			prompt = c.Text()
+		)
+		// Use full-fledged bot's functions
+		// only if you need a result:
+		_, err := b.Send(tgUser, prompt)
+		if err != nil {
+			return err
+		}
+		//msg = nil
 
-        // Instead, prefer a context short-hand:
-        id := fmt.Sprintf("%d", user.ID)
-        return c.Send("LANG: " + user.LanguageCode + " USER: " + id + " | "+ user.Username + " TEXT: " + text)
-    })
+		//tgID tgUser.ID
 
-    b.Handle(tele.OnQuery, func(c tele.Context) error {
-        results := make(tele.Results, 1, 1) // []tele.Result
-        result := &tele.PhotoResult{
-			      URL:      "https://image.jpg",
-			      ThumbURL: "https://thumb.jpg", // required for photos
-		    }
-        results[0] = result
-	      // Incoming inline queries.
-        return c.Answer(
-            &tele.QueryResponse{
-                Results: results,
-            })
-    })
+		var user User
+		var ok bool
+		if user, ok = users[tgUser.ID]; !ok {
+			users[tgUser.ID] = User{
+				ID:        "",
+				TGID:      tgUser.ID,
+				Mode:      "chat",
+				SessionID: uuid.New().String(),
+				Status:    "",
+			}
+		}
 
-    b.Start()
+		//res, err := http.Get(requestURL)
+		//if err != nil {
+		//    fmt.Printf("error making http request: %s\n", err)
+		//    os.Exit(1)
+		//}
+
+		jsonBody := []byte(`{"id": "` + user.SessionID + `", "prompt": "` + prompt + `"}`)
+		bodyReader := bytes.NewReader(jsonBody)
+
+		requestURL := os.Getenv("FAST") + "/jobs"
+		req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+		if err != nil {
+			fmt.Printf("client: could not create request: %s\n", err)
+			os.Exit(1)
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		client := http.Client{
+			Timeout: 30 * time.Second,
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("client: error making http request: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%+v", res)
+
+		// Instead, prefer a context short-hand:
+		//id := fmt.Sprintf("%d", user.TGID)
+		//return c.Send("LANG: " + user.LanguageCode + " USER: " + id + " | " + user.Username + " TEXT: " + text)
+
+		return c.Send("OK")
+	})
+
+	b.Handle(tele.OnQuery, func(c tele.Context) error {
+		//var (
+		//	user = c.Sender()
+		//	text = c.Text()
+		//)
+
+		results := make(tele.Results, 1, 1) // []tele.Result
+		result := &tele.PhotoResult{
+			URL:      "https://image.jpg",
+			ThumbURL: "https://thumb.jpg", // required for photos
+		}
+		results[0] = result
+		// Incoming inline queries.
+		return c.Answer(
+			&tele.QueryResponse{
+				Results: results,
+			})
+	})
+
+	fmt.Printf("\nListen for Telegram...")
+	b.Start()
 }
